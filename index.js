76803ae56d859dad625936fc8b611c4aad71d3ce
Christian <cedenoc181@gmail.com>
2022-09-04 17:36:23 -0400

    commit tree 3dae2eb1b62cbc8f383f31913e9479410424a425

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield'];
+
+ function destructivelyAppendCat(){
+    return cats.push('Ralph');
+ }
+ 
+function  destructivelyPrependCat (){
+    return cats.unshift('Bob');
+} 
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function  appendCat(Broom) {
+    const newName = [...cats,'Broom'];
+    return newName;
+}
+
+function  prependCat(Arnold) {
+const newCat = ['Arnold', ...cats];
+return newCat;
+}
+
+function removeLastCat() {
+    return cats.slice(0,2);
+}
+
+function  removeFirstCat() {
+    return cats.slice(1);
+}
--- test/helpers.js
+++ test/helpers.js
@@ -19,3 +19,56 @@ const src = babelResult.code
 jsdom({
   html, src
 });
+
+
+
+
+
+
+// Write your solution here!
+const cats = [
+  "Milo", "Otis", "Garfield"
+];
+
+
+function destructivelyAppendCat(_Ralph){
+  cats.push(_Ralph);
+  return cats;
+}
+
+
+function destructivelyPrependCat(_Bob){
+  cats.unshift(_Bob);
+  return cats;
+}
+
+
+function destructivelyRemoveLastCat(_Garfield){
+cats.pop(_Garfield);
+return cats;
+}
+
+function destructivelyRemoveFirstCat(_Milo){
+  cats.shift(_Milo);
+  return cats;
+}
+
+function appendCat(_Broom) {
+  const newCats = [...cats, _Broom]
+  return newCats;
+}
+
+function prependCat(_Arnold) {
+  const delCats = [_Arnold, ...cats];
+  return delCats;
+}
+
+function removeLastCat(){
+  const remCat = cats.slice(0,cats.length-1);
+  return remCat;
+}
+function removeFirstCat() {
+  const fCats = cats.slice(1);
+  return fCats;
+
+}
